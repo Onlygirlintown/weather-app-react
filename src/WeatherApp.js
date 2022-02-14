e9a8837c838b8f2bd4b5fe27ab4a6413f140ac86
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-02-14 15:25:47 -0800

    added AXIOS to current day

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./WeatherApp.css";
 
 export default function WeatherApp() {
-  function handleResponse(response) {
-    alert(response.data.main);
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [wind, setWind] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [description, setDescription] = useState("");
+  const [icon, setIcon] = useState("");
+
+  function displayWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+    setDescription(response.data.weather[0].description);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
   }
 
-  let city = "New York";
-  let apiKey = "ff603b4615415a7ed5f7b26e07b59db6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "ff603b4615415a7ed5f7b26e07b59db6";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           className="formInput"
           placeholder="type in a city here!"
+          onChange={updateCity}
         />
         <button type="submit" className="btn btn-primary">
           Search
@@ -27,14 +47,14 @@ export default function WeatherApp() {
       </form>
       <div className="row">
         <div className="col-6 CurrentDayInfo">
-          <h1>Monday!!!!</h1>
-          <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
-          <span>70°F</span>
+          <h1>Monday</h1>
+          <img src={icon} />
+          <span>{temperature}F/C</span>
           <ul className="CurrentDayStats">
             <li>12:00pm</li>
-            <li>Clear Skies!!!</li>
-            <li>Wind= 10mph</li>
-            <li>Humidty: 10%</li>
+            <li className="text-capitalize">{description}</li>
+            <li>Wind: {wind} mph</li>
+            <li>Humidty: {humidity}%</li>
           </ul>
         </div>
       </div>
