fc6fac5a1ba44b88379be2ce09e7280690aea431
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-02-16 12:22:32 -0800

    restructured States within function

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,69 +4,65 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./WeatherApp.css";
 
 export default function WeatherApp() {
-  const [city, setCity] = useState("");
-  const [temperature, setTemperature] = useState("");
-  const [wind, setWind] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [description, setDescription] = useState("");
-  const [icon, setIcon] = useState("");
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
   function displayWeather(response) {
-    setTemperature(Math.round(response.data.main.temp));
-    setHumidity(response.data.main.humidity);
-    setWind(Math.round(response.data.wind.speed));
-    setDescription(response.data.weather[0].description);
-    setIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+    console.log(response.data);
+
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      humidity: Math.round(response.data.main.humidity),
+      description: response.data.weather[0].description,
+      city: response.data.name,
+      date: "Monday 12:00pm",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  if (ready) {
+    return (
+      <div className="container">
+        <form>
+          <input
+            type="search"
+            className="formInput"
+            placeholder="type in a city here!"
+            autoFocus="on"
+          />
+          <button type="submit" className="btn btn-primary">
+            🔎
+          </button>
+        </form>
+        <div className="row">
+          <div className="col-6 CurrentDayInfo">
+            <h1 className="text-capitalize">{weatherData.city}</h1>
+            <img src={weatherData.iconUrl} />
+            <span className="temperatureDegree">{weatherData.temperature}</span>
+            <span className="degreeSymbol">°</span>
+            <span>
+              <a href="/" className="celsiusDegree">
+                C
+              </a>
+            </span>
+            <ul className="CurrentDayStats">
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+              <li>Wind: {weatherData.wind} mph</li>
+              <li>Humidty: {weatherData.humidity} %</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let city = "Miami";
     let apiKey = "ff603b4615415a7ed5f7b26e07b59db6";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(url).then(displayWeather);
-  }
 
-  function updateCity(event) {
-    setCity(event.target.value);
+    return "Loading!";
   }
-
-  return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          className="formInput"
-          placeholder="type in a city here!"
-          onChange={updateCity}
-        />
-        <button type="submit" className="btn btn-primary">
-          Search!
-        </button>
-      </form>
-      <div className="row">
-        <div className="col-6 CurrentDayInfo">
-          <h1 className="text-capitalize">{city}</h1>
-          <img src={icon} />
-          <span className="temperatureDegree">{temperature}</span>
-          <span className="degreeSymbol">°</span>
-          <span>
-            <a href="#" className="units">
-              F/
-              <a href="#" className="celsiusDegree">
-                C
-              </a>
-            </a>
-          </span>
-          <ul className="CurrentDayStats">
-            <li>12:00pm</li>
-            <li className="text-capitalize">{description}</li>
-            <li>Wind: {wind} mph</li>
-            <li>Humidty: {humidity}%</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
 }
